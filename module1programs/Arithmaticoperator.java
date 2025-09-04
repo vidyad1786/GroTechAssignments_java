773211e8322d392f32bfb0831abaefe751ea0ec8
Priya Kulkarni <vidyad1786@gmail.com>
2025-09-04 18:49:45 +0530

    Update Arithmaticoperator.java

--- module1programs/Arithmaticoperator.java
+++ module1programs/Arithmaticoperator.java
@@ -1,1 +1,37 @@
+package module1programs;
 
+public class Arithmaticoperator {
+
+	public static void main(String[] args) {
+		
+		int a=12;
+		int b= 20;
+		int c=a+b;
+		System.out.println(c);
+		
+		int d=12;
+		int e= 20;
+		int f=e-d;
+		System.out.println(f);
+		
+		int g=12;
+		int h= 20;
+		int i=g*h;
+		System.out.println(i);
+		
+		int j=12;
+		int k= 20;
+		int l=j/k;
+		System.out.println(l);
+
+		int m=20;
+		int n= 2;
+		int p=m%n;
+		System.out.println(p);
+
+
+
+
+	}
+
+}
